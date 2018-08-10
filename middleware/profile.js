ba1418f227a62589a709976715634d1380b81c53
shrpne <shrpne@gmail.com>
2018-08-10 19:43:01 +0300

    fix Unauthorized error

--- middleware/profile.js
+++ middleware/profile.js
@@ -1,10 +1,17 @@
-export default function ({store}) {
+export default function ({store, redirect}) {
     if (process.server) {
         return;
     }
     if (store.getters.isUserWithProfile) {
         // wait for profile, bc its data need for all pages
-        return store.dispatch('FETCH_PROFILE');
+        return store.dispatch('FETCH_PROFILE')
+            .catch((error) => {
+                // Unauthorized: logout bc. auth data is not approved by server
+                if (error.response.status === 401) {
+                    store.commit('LOGOUT');
+                    redirect('/');
+                }
+            })
     }
 
     return Promise.resolve();
