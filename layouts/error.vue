4f0c62004ee80c17fdb25540012bdd4a5394b687
shrpne <shrpne@gmail.com>
2019-02-07 18:10:57 +0300

    update error message

--- layouts/error.vue
+++ layouts/error.vue
@@ -7,14 +7,15 @@
 </script>
 
 <template>
-    <div class="error__container u-container u-container--small">
-        <div class="error__section u-section">
-            <h1 class="u-h1--xl error__title">Ошибка {{ error.statusCode }}</h1>
-            <p v-if="error.statusCode === 404">К сожалению, здесь ничего нет. Проверьте правильность адреса или просто перейдите на главную страницу нашего журнала.</p>
-            <p v-else-if="error.message === 'Auth error'">Ошибка авторизации</p>
-            <p v-else-if="error.message === 'Network Error'">Сетевая ошибка</p>
-            <p v-else>Что-то пошло не так</p>
-            <a class="error__button mdc-button mdc-button--outlined" href="/">Перейти на главную</a>
+    <div class="u-container u-container--small">
+        <div class="u-section">
+            <h1 class="">{{ tt('Error', 'error.title-error') }} {{ error.statusCode }}</h1>
+            <p v-if="error.statusCode === 404">{{ tt('Page not found', 'error.message-404') }}</p>
+            <p v-else-if="error.statusCode === 504" v-html="tt('Request failed with status code 504. <br> Gateway time-out.', 'error.message-504')"></p>
+            <p v-else-if="error.statusCode === 503" v-html="tt('The webpage is currently unavailable. <br> It may be overloaded or down for maintenance.', 'error.message-503')"></p>
+            <p v-else-if="error.message === 'Network Error'">{{ tt('Network Error', 'error.message-network') }}</p>
+            <p v-else>{{ error.message }}</p>
+            <p><a class="button button--ghost" href="/">{{ tt('Return to main page', 'error.return-link') }}</a></p>
         </div>
     </div>
 
