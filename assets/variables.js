9238b8f6cd687e19df2220f844b44983070673e7
shrpne <shrpne@gmail.com>
2019-04-22 16:08:58 +0300

    minor fix

--- assets/variables.js
+++ assets/variables.js
@@ -6,7 +6,7 @@ export const BASE_TITLE_END = ' — Minter';
 export const BASE_TITLE = BASE_TITLE_NETWORK + 'Console' + BASE_TITLE_END;
 export const BASE_DESCRIPTION = `Minter Console is by far the most advanced part of our project that lets you manage all your activities on our ${NETWORK === TESTNET ? 'test ': ''}network.`;
 export const MINTER_ACCOUNTS_API_URL = process.env.APP_MINTER_ACCOUNTS_URL + '/api/v1/';
-export const COIN_NAME = process.env.APP_ENV === 'production' ? 'BIP' : 'MNT';
+export const COIN_NAME = NETWORK === MAINNET ? 'BIP' : 'MNT';
 export const EXPLORER_URL = process.env.APP_EXPLORER_URL;
 export const EXPLORER_API_URL = process.env.APP_EXPLORER_API_URL + '/api/v1/';
 export const MINTER_GATE_URL = process.env.APP_MINTER_GATE_URL;
