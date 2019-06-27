c7d8b4660d3dd5d73fecd21d725a3648409cd2ae
shrpne <shrpne@gmail.com>
2019-06-27 20:44:39 +0300

    satisfy eslint

--- lang/en.js
+++ lang/en.js
@@ -1,5 +1,5 @@
 export default {
     form: {
         'delegation-unbond-confirm-description': 'Are you sure you want to unbond your coins? They will return to your address in approximately <strong>30&nbsp;days</strong> (518&#x202F;400 blocks) and will not be generating rewards during this period.',
-    }
+    },
 };
