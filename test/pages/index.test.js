2ff82d2e1e55ddd5aa98a70d78afeb30bbc3710b
shrpne <shrpne@gmail.com>
2019-09-07 19:39:31 +0300

    fix tests

--- test/pages/index.test.js
+++ test/pages/index.test.js
@@ -41,7 +41,7 @@ describe('private routes redirect', () => {
 
 describe('index page', () => {
     /** @type {ItConcurrent} */
-    const testnetOnly = NETWORK === TESTNET ? test : test.skip;
+    const testnetBranchOnly = process.env.APP_BRANCH_ENV === TESTNET ? test : test.skip;
 
     beforeAll(async () => {
         await page.goto(ROUTES.public.index);
@@ -51,7 +51,7 @@ describe('index page', () => {
         await page.waitForSelector('[data-test-id="authSection"]');
     });
 
-    testnetOnly('register new user and redirect to wallet', async () => {
+    testnetBranchOnly('register new user and redirect to wallet', async () => {
         // console.log({user});
         await page.type('[data-test-id="authRegisterInputName"]', user.username);
         await page.type('[data-test-id="authRegisterInputPassword"]', user.password);
@@ -62,11 +62,11 @@ describe('index page', () => {
         await page.waitForSelector('[data-test-id="walletAddressLink"]');
     }, 30000);
 
-    testnetOnly('logout and redirect to auth form', () => {
+    testnetBranchOnly('logout and redirect to auth form', () => {
         return logout(page);
     });
 
-    testnetOnly('login and redirect to wallet', async () => {
+    testnetBranchOnly('login and redirect to wallet', async () => {
         await page.type('[data-test-id="authLoginInputName"]', user.username);
         await page.type('[data-test-id="authLoginInputPassword"]', user.password);
         // submit
@@ -75,7 +75,7 @@ describe('index page', () => {
         await page.waitForSelector('[data-test-id="walletAddressLink"]');
     }, 30000);
 
-    testnetOnly('logout and redirect to auth form', () => {
+    testnetBranchOnly('logout and redirect to auth form', () => {
         return logout(page);
     });
 
