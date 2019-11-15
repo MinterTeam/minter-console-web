2bc3bc936136e15ec60758667474430d8428e7b5
shrpne <shrpne@gmail.com>
2019-11-15 16:47:05 +0300

    fix testnet notice emoji

--- components/common/TestnetNotice.vue
+++ components/common/TestnetNotice.vue
@@ -6,9 +6,9 @@
 <template>
     <div class="testnet-notice">
         <div class="testnet-notice__container u-container u-container--large">
-            <span class="testnet-notice__icon">⚠️</span>
+            <span class="testnet-notice__icon u-emoji">⚠️</span>
             <span class="testnet-notice__caption">Attention! <span class="u-display-ib">This is a testnet version</span></span>
-            <span class="testnet-notice__icon">⚠️</span>
+            <span class="testnet-notice__icon u-emoji">⚠️</span>
         </div>
     </div>
 </template>
