04243c0649d262da002147e8c059020351deda0a
shrpne <shrpne@gmail.com>
2020-09-26 18:53:54 +0300

    fix build

--- api/gate.js
+++ api/gate.js
@@ -4,7 +4,7 @@ import PostSignedTx from 'minter-js-sdk/src/api/post-signed-tx';
 import GetNonce from 'minter-js-sdk/src/api/get-nonce';
 import EstimateCoinSell from 'minter-js-sdk/src/api/estimate-coin-sell';
 import EstimateCoinBuy from 'minter-js-sdk/src/api/estimate-coin-buy';
-import ReplaceCoinSymbol, {ReplaceCoinSymbolByPath} from 'minter-js-sdk/src/api/replace-coin-symbol.js';
+import {ReplaceCoinSymbol, ReplaceCoinSymbolByPath} from 'minter-js-sdk/src/api/replace-coin.js';
 import {GATE_API_URL, CHAIN_ID} from '~/assets/variables';
 
 const minterApi = new MinterApi({apiType: 'gate', baseURL: GATE_API_URL, chainId: CHAIN_ID});
