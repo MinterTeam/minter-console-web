50486f6a234cb81da70dd7028f1996ed7b13d21c
danil-lashin <danil-lashin@users.noreply.github.com>
2021-12-06 13:34:41 +0000

    deploy: f8451bee30de69f3047debc3aee46a391c4bc7a8

---
+++ _nuxt/57b03df.modern.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{1137:function(e,n,r){"use strict";r.r(n);var d={middleware(e){var{redirect:n,app:r}=e;return n(r.i18nGetPreferredPath("swap"))}},t=r(42),component=Object(t.a)(d,undefined,undefined,!1,null,null,null);n.default=component.exports}}]);
--- _nuxt/a693511.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{1229:function(e,n,r){"use strict";r.r(n);var d={middleware:function(e){return(0,e.redirect)(e.app.i18nGetPreferredPath("swap"))}},t=r(56),component=Object(t.a)(d,undefined,undefined,!1,null,null,null);n.default=component.exports}}]);
