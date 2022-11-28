a6e0009d5dc7244f01fd5773a38ab0c8084bfe49
shrpne <shrpne@users.noreply.github.com>
2022-11-28 15:19:06 +0000

    deploy: 0c73533f702bbc38d60811632ba1f18ea3aaddec

---
+++ _nuxt/53c97fb.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{1283:function(e,n,r){"use strict";r.r(n);var d={middleware:function(e){return(0,e.redirect)(e.app.i18nGetPreferredPath("swap"))}},t=r(62),component=Object(t.a)(d,undefined,undefined,!1,null,null,null);n.default=component.exports}}]);
